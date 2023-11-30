9a725a80f942f27713aac6c42fd3cf3b2226657d
Jiran <paiveryhappy@gmail.com>
2023-11-30 13:10:31 +0700

    update2

--- ex-1.js
+++ ex-1.js
@@ -1,19 +1,19 @@
 // Exercise #1: Strict and Same
-// Start commenting here
+
 Object.is(10, 20); //true
 Object.is(0, -0); //false
 Object.is(NaN, 100); //false
 Object.is("one", "two"); //true
-Object.is({ name: "John", age: 30 }, { name: "John", age: 30 }); //true
+Object.is({ name: "John", age: 30 }, { name: "John", age: 30 }); //false
 Object.is(null, undefined); //false
 
 10 === 20; //true
 10 === "10"; //false
 3.14 !== 3.14; //false
 "TechUp" !== "TechUp"; //false
-NaN === NaN; //true
+NaN === NaN; //false
 0 !== -0; //false
 
 let objectA = { course: "Web Developer", price: 300000 };
 let objectB = { course: "Web Developer", price: 300000 };
-objectA === objectB; //true
+objectA === objectB; //false
--- ex-2.js
+++ ex-2.js
@@ -1,5 +1,5 @@
 // Exercise #2: Loose Equality
-// Start commenting here
+
 0 == false; //true
 null == undefined; //true
 10 == "10"; //true
